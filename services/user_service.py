0a3aca39a907f956b7026745739c621a397967ff
Abhijit Gaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2026-01-30 12:36:48 +0530

    dummy commit 12

--- services/user_service.py
+++ services/user_service.py
@@ -3,7 +3,7 @@ from models.user_model import users
 def create_user_service(data):
     user_id = data.get("id")
     if user_id in users:
-        return {"message": "User already exists"}, 400
+        return {"message": "User is present"}, 400
 
     # users[user_id] = {
     #     "name": data.get("name"),
@@ -31,11 +31,6 @@ def update_user_service(user_id, data):
 
     return {"message": "User updated", "user": users[user_id]}, 200
 
-def delete_user_service(user_id):
-    if user_id not in users:
-        return {"message": "User not found"}, 404
-    del users[user_id]
-    return {"message": "User deleted"}, 200
 
 def even_odd_service(user_id):
     user = users.get(user_id)
