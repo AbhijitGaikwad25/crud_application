539f7350ac72bb6616a4f6c59c133561b58a27dc
Abhijit Gaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2026-02-02 12:11:06 +0000

    demo check 211

--- services/user_service.py
+++ services/user_service.py
@@ -1,44 +1,49 @@
 from models.user_model import users
 
 def create_user_service(data):
-    user_id = data.get("id")
-    if user_id in users:
-        return {"message": "User already exists"}, 400
+    try:
+        user_id = data.get("id")
+        if user_id in users:
+            return {"message": "User already exists"}, 400
 
-    # users[user_id] = {
-    #     "name": data.get("name"),
-    #     "email": data.get("email")
-    # }
-    return {"message": "User created", "user": users[user_id]}, 201
+        users[user_id] = {
+            "name": data.get("name"),
+            "email": data.get("email")
+        }
+        return {"message": "User created", "user": users[user_id]}, 201
+    except Exception as e:
+        return {"message": "An error occurred: " + str(e)}, 500
 
 def get_user_service(user_id):
-    user = users.get(user_id)
-    if not user:
-        return {"message": "User not found"}, 404
-    return user, 200
+    try:
+        user = users.get(user_id)
+        if not user:
+            return {"message": "User not found"}, 404
+        return user, 200
+    except Exception as e:
+        return {"message": "An error occurred: " + str(e)}, 500
 
 def get_all_users_service():
-    return users, 200
-
-def update_user_service(user_id, data):
-    if user_id not in users:
-        return {"message": "User not found"}, 404
-
-    # if "name" in data:
-    #     users[user_id]["name"] = data["name"]
-    # if "email" in data:
-    #     users[user_id]["email"] = data["email"]
-
-    return {"message": "User updated", "user": users[user_id]}, 200
+    try:
+        return users, 200
+    except Exception as e:
+        return {"message": "An error occurred: " + str(e)}, 500
 
 def delete_user_service(user_id):
-    if user_id not in users:
-        return {"message": "User not found"}, 404
-    del users[user_id]
-    return {"message": "User deleted"}, 200
+    try:
+        if user_id not in users:
+            return {"message": "User not found"}, 404
+        del users[user_id]
+        return {"message": "User deleted successfully"}, 200
+    except Exception as e:
+        return {"message": "An error occurred: " + str(e)}, 500
 
-def even_odd_service(user_id):
-    user = users.get(user_id)
-    if not user:
-        return {"message": "User not found"}, 404
-    return {"user_id": user_id, "is_even": user_id % 2 == 0}, 200
+def get_odd_users_service():
+    try:
+        odd_users = {user_id: user for user_id, user in users.items() if int(user_id) % 2 != 0}
+        return odd_users, 200
+    except Exception as e:
+        return {"message": "An error occurred: " + str(e)}, 500
+
+def hello_world():
+    return {"message": "Hello , World!"}, 200
