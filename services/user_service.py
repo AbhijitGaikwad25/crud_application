626e277c601830a8bb90711aea271a7abe3a0f93
Abhijit Gaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2026-02-06 09:04:52 +0000

    added update_user_service and update_user route

--- services/user_service.py
+++ services/user_service.py
@@ -38,14 +38,15 @@ def delete_user_service(user_id):
     except Exception as e:
         return {"message": "An error occurred: " + str(e)}, 500
 
+
 def update_user_service(user_id, data):
     try:
         if user_id not in users:
             return {"message": "User not found"}, 404
-        if "name" in data:
-            users[user_id]["name"] = data["name"]
-        if "email" in data:
-            users[user_id]["email"] = data["email"]
+        users[user_id].update({
+            "name": data.get("name", users[user_id]["name"]),
+            "email": data.get("email", users[user_id]["email"])
+        })
         return {"message": "User updated successfully", "user": users[user_id]}, 200
     except Exception as e:
-        return {"message ": "An error occurred: " + str(e)}, 500
+        return {"message": "An error occurred: " + str(e)}, 500
